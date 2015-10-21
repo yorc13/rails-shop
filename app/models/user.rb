08370b276dacab4d143e69c3daa3aad1dc19c2ed
Dmitrii Tsyganov <sylvix.dh@gmail.com>
2015-10-21 20:01:19 +0600

    Added field validations to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :trackable
+         :rememberable, :trackable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true
 end
