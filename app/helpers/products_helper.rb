0924034d8bbb6d0e714507e701ecff911bc3f867
Dmitrii Tsyganov <sylvix.dh@gmail.com>
2015-10-21 23:28:28 +0600

    Added "coming soon" image if product image doesn't exist. Added helper to show product image according to this.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+  def product_image(id)
+    if File.exists? Rails.root.join('public', 'uploads', id.to_s)
+      "/uploads/#{id}"
+    else
+      'coming-soon.png'
+    end
+  end
 end
