b620929befe7f0cbf484bd3370ddc0c485661896
Dmitrii Tsyganov <sylvix.dh@gmail.com>
2015-10-22 01:45:47 +0600

    Removed couple of old tests files of controllers that were renamed manually.

--- test/controllers/shop_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ShopControllerTest < ActionController::TestCase
-  test "should get products_index" do
-    get :products_index
-    assert_response :success
-  end
-
-end
