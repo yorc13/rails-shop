92b732a066c9f9d79f675091c841d4611f3c3f54
Андрей <yorc13@gmail.com>
2015-10-23 19:58:51 +0600

    Removed admin controllers

--- app/controllers/admin_controller.rb
+++
@@ -1,5 +0,0 @@
-class AdminController < ApplicationController
-  layout 'admin'
-
-  before_action :authenticate_user!, :admin_user
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
   root 'products#index'
-
   devise_for :users
-
-  namespace :admin do
-    root 'products#index'
-    resources :products
-    resources :categories
-  end
 end
